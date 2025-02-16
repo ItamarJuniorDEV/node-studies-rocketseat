6225bc322bb8975ebda75c63a95801112d25ccfa
Itamar Junior <cdajuniorf@gmail.com>
2025-02-15 21:53:37 -0300

    feat: Url da requisição;

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  const { method } = req;
+  const { method, url } = req;
 
-  return res.writeHead(404).end("Método: " + method);
+  if (method === "GET" && url === "/products") {
+    return res.end("Lista de produtos");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return res.writeHead(201).end("Produto cadastrado!");
+  }
+
+  return res.writeHead(404).end("Rota não encontrada!");
 });
 
 server.listen(3333);
