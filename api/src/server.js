d72c77bc3be3f79edd6c07641ec769765d9c0499
Itamar Junior <cdajuniorf@gmail.com>
2025-02-15 21:37:56 -0300

    Começo dos estudos

---
+++ api/src/server.js
@@ -0,0 +1,9 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  const { method } = req;
+
+  return res.writeHead(404).end("Método: " + method);
+});
+
+server.listen(3333);
