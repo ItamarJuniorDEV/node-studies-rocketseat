f050b295e6714517bb5780a3527a904b4ce22cc2
Itamar Junior <cdajuniorf@gmail.com>
2025-02-20 20:12:07 -0300

    feat: Criando o arquivo.

--- api/src/database.js
+++ api/src/database.js
@@ -1,17 +1,26 @@
-export class Database {
-  database = {}
+import fs from "node:fs/promises";
 
-  insert(table, data){
-    if(Array.isArray(this.database[table])){
-      this.database[table].push(data)
+const DATABASE_PATH = new URL("./db.json", import.meta.url);
+export class Database {
+  database = {};
+
+  constructor() {
+    this.persist();
+  }
+
+  persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database));
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.database[table])) {
+      this.database[table].push(data);
     } else {
-      this.database[table] = [data]
+      this.database[table] = [data];
     }
   }
 
-    select(table){
-      return this.database[table]
-    }
+  select(table) {
+    return this.database[table];
+  }
 }
-
-
